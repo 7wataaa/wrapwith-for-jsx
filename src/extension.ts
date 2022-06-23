ba4afc63738f772f2e6facd58b76822f45692b1f
nashi <nn.contact.n@gmail.com>
2022-06-24 00:30:21 +0900

    一時退避

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,196 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { parse } from '@babel/parser';
+import { default as traverse, NodePath, Node } from '@babel/traverse';
+import type {
+  JSXElement,
+  JSXFragment,
+  ArrowFunctionExpression,
+  BlockStatement,
+  ReturnStatement,
+} from '@babel/types';
+import {
+  isJSXElement,
+  isJSXFragment,
+  isArrowFunctionExpression,
+  isBlockStatement,
+  isReturnStatement,
+} from '@babel/types';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const BALANCE_OUT_COMMAND = 'editor.emmet.action.balanceOut';
+const WRAP_WITH_TAG_COMMAND = 'wrapwith-for-jsx.wrapWithTag';
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wrapwith-for-jsx" is now active!');
+  console.log(
+    'Congratulations, your extension "wrapwith-for-jsx" is now active!',
+    new Date(Date.now()).toLocaleDateString()
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('wrapwith-for-jsx.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from wrapWith for JSX!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand(WRAP_WITH_TAG_COMMAND, () => {
+      vscode.window.showInformationMessage('WRAP_WITH_TAG_COMMAND');
+    })
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      ['javascriptreact', 'typescriptreact'],
+      new TagWraper()
+    )
+  );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class TagWraper implements vscode.CodeActionProvider {
+  static alreadyActivateCheckPosition: vscode.Position;
+
+  public static readonly providedCodeActionKinds = [
+    vscode.CodeActionKind.QuickFix,
+  ];
+
+  public provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range
+  ): vscode.CodeAction[] | null {
+    if (!this.showQuickFix(document)) {
+      return null;
+    }
+
+    // 何も選択されていない状態と選択されている状態とで処理を分ける
+
+    const wrapWithFix = new vscode.CodeAction(
+      'Wrap with ...',
+      vscode.CodeActionKind.QuickFix
+    );
+
+    wrapWithFix.command = {
+      title: 'WRAP_WITH_TAG_COMMAND',
+      command: WRAP_WITH_TAG_COMMAND,
+    };
+
+    return [wrapWithFix];
+  }
+
+  private showQuickFix(document: vscode.TextDocument): boolean {
+    const isTypeScriptReact = document.languageId === 'typescriptreact';
+    const isJavaScriptReact = document.languageId === 'javascriptreact';
+
+    if (!isTypeScriptReact && !isJavaScriptReact) {
+      return false;
+    }
+
+    const parseResult = this.tryParse(document.getText());
+
+    if (!parseResult) {
+      return false;
+    }
+
+    const positions = this.findAllJSXElementPosition(
+      parseResult,
+      new SearchQue()
+    );
+
+    console.log('positions: ', positions);
+
+    return true;
+  }
+
+  private tryParse(srcText: string) {
+    try {
+      const ast = parse(srcText, {
+        plugins: ['typescript', 'jsx', 'throwExpressions'],
+        sourceType: 'unambiguous',
+      });
+
+      return ast;
+    } catch (e) {
+      console.log('=========ERROR=========');
+      console.error(e);
+
+      return null;
+    }
+  }
+
+  private findAllJSXElementPosition(
+    programAST: Node | Node[] | null | undefined,
+    que: SearchQue
+  ): { start: number; end: number }[] {
+    if (programAST === null || programAST === undefined) {
+      console.error('programASTが空');
+
+      return [];
+    }
+
+    try {
+      traverse(programAST, {
+        enter: (path) => {
+          if (
+            path.isJSXElement() ||
+            path.isJSXFragment() ||
+            path.isArrowFunctionExpression() ||
+            path.isBlockStatement() ||
+            path.isReturnStatement()
+            // TODO classへの対応
+          ) {
+            que.push(path);
+          }
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    console.log('que: ', que.length());
+
+    const result: { start: number; end: number; path: NodePath }[] = [];
+
+    while (que.length() > 0) {
+      const e = que.pop();
+
+      if (e.isJSXElement() || e.isJSXFragment()) {
+        result.push({
+          start: e.node.start ?? -1,
+          end: e.node.end ?? -1,
+          path: e,
+        });
+      } else if (isArrowFunctionExpression(e)) {
+      } else if (isBlockStatement(e)) {
+      }
+    }
+
+    return result;
+  }
+}
+
+class SearchQue {
+  private data: (
+    | NodePath<JSXElement>
+    | NodePath<JSXFragment>
+    | NodePath<ArrowFunctionExpression>
+    | NodePath<BlockStatement>
+    | NodePath<ReturnStatement>
+  )[] = [];
+
+  public length() {
+    return this.data.length;
+  }
+
+  public push(
+    item:
+      | NodePath<JSXElement>
+      | NodePath<JSXFragment>
+      | NodePath<ArrowFunctionExpression>
+      | NodePath<BlockStatement>
+      | NodePath<ReturnStatement>
+  ) {
+    this.data.push(item);
+  }
+
+  public pop() {
+    const front = this.data.shift();
+
+    if (!front) {
+      throw new Error('Queueが空です');
+    }
+
+    return front;
+  }
+}
